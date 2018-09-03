26bea6deab8056c12521d14e8f330f98d4bd21df
Maicon Canedo <maicon.canedo2013@outlook.com>
2018-09-02 22:21:34 -0300

    Create DTOs Getters and Setters

--- src/br/org/faetectcc2018/dto/BemCandidato.java
+++ src/br/org/faetectcc2018/dto/BemCandidato.java
@@ -10,4 +10,70 @@ public class BemCandidato {
     private String dsTipoBemCandidato;
     private String detalheBem;
     private String valorBem;
+
+    public BemCandidato() {}
+
+    public String getAnoEleicao() {
+        return anoEleicao;
+    }
+
+    public void setAnoEleicao(String anoEleicao) {
+        this.anoEleicao = anoEleicao;
+    }
+
+    public String getDescricaoEleicao() {
+        return descricaoEleicao;
+    }
+
+    public void setDescricaoEleicao(String descricaoEleicao) {
+        this.descricaoEleicao = descricaoEleicao;
+    }
+
+    public String getSiglaUf() {
+        return siglaUf;
+    }
+
+    public void setSiglaUf(String siglaUf) {
+        this.siglaUf = siglaUf;
+    }
+
+    public String getSqCandidato() {
+        return sqCandidato;
+    }
+
+    public void setSqCandidato(String sqCandidato) {
+        this.sqCandidato = sqCandidato;
+    }
+
+    public String getCdTipoBemCandidato() {
+        return cdTipoBemCandidato;
+    }
+
+    public void setCdTipoBemCandidato(String cdTipoBemCandidato) {
+        this.cdTipoBemCandidato = cdTipoBemCandidato;
+    }
+
+    public String getDsTipoBemCandidato() {
+        return dsTipoBemCandidato;
+    }
+
+    public void setDsTipoBemCandidato(String dsTipoBemCandidato) {
+        this.dsTipoBemCandidato = dsTipoBemCandidato;
+    }
+
+    public String getDetalheBem() {
+        return detalheBem;
+    }
+
+    public void setDetalheBem(String detalheBem) {
+        this.detalheBem = detalheBem;
+    }
+
+    public String getValorBem() {
+        return valorBem;
+    }
+
+    public void setValorBem(String valorBem) {
+        this.valorBem = valorBem;
+    }
 }
--- src/br/org/faetectcc2018/dto/Candidato.java
+++ src/br/org/faetectcc2018/dto/Candidato.java
@@ -46,4 +46,358 @@ public class Candidato {
     private String descSitTotTurno;
     private String nmEmail;
     private String dataHoraGeracao;
+
+    public Candidato() {}
+
+    public String getAnoEleicao() {
+        return anoEleicao;
+    }
+
+    public void setAnoEleicao(String anoEleicao) {
+        this.anoEleicao = anoEleicao;
+    }
+
+    public String getNumTruno() {
+        return numTruno;
+    }
+
+    public void setNumTruno(String numTruno) {
+        this.numTruno = numTruno;
+    }
+
+    public String getDescricaoEleicao() {
+        return descricaoEleicao;
+    }
+
+    public void setDescricaoEleicao(String descricaoEleicao) {
+        this.descricaoEleicao = descricaoEleicao;
+    }
+
+    public String getSiglaUf() {
+        return siglaUf;
+    }
+
+    public void setSiglaUf(String siglaUf) {
+        this.siglaUf = siglaUf;
+    }
+
+    public String getSiglaUE() {
+        return siglaUE;
+    }
+
+    public void setSiglaUE(String siglaUE) {
+        this.siglaUE = siglaUE;
+    }
+
+    public String getCodigoCargo() {
+        return codigoCargo;
+    }
+
+    public void setCodigoCargo(String codigoCargo) {
+        this.codigoCargo = codigoCargo;
+    }
+
+    public String getDescricaoCargo() {
+        return descricaoCargo;
+    }
+
+    public void setDescricaoCargo(String descricaoCargo) {
+        this.descricaoCargo = descricaoCargo;
+    }
+
+    public String getNomeCandidato() {
+        return nomeCandidato;
+    }
+
+    public void setNomeCandidato(String nomeCandidato) {
+        this.nomeCandidato = nomeCandidato;
+    }
+
+    public String getSequencialCandidato() {
+        return sequencialCandidato;
+    }
+
+    public void setSequencialCandidato(String sequencialCandidato) {
+        this.sequencialCandidato = sequencialCandidato;
+    }
+
+    public String getNumeroCandidato() {
+        return numeroCandidato;
+    }
+
+    public void setNumeroCandidato(String numeroCandidato) {
+        this.numeroCandidato = numeroCandidato;
+    }
+
+    public String getCpfCandidato() {
+        return cpfCandidato;
+    }
+
+    public void setCpfCandidato(String cpfCandidato) {
+        this.cpfCandidato = cpfCandidato;
+    }
+
+    public String getNomeUrnaCandidato() {
+        return nomeUrnaCandidato;
+    }
+
+    public void setNomeUrnaCandidato(String nomeUrnaCandidato) {
+        this.nomeUrnaCandidato = nomeUrnaCandidato;
+    }
+
+    public String getCodSituacaoCandidatura() {
+        return codSituacaoCandidatura;
+    }
+
+    public void setCodSituacaoCandidatura(String codSituacaoCandidatura) {
+        this.codSituacaoCandidatura = codSituacaoCandidatura;
+    }
+
+    public String getDesSituacaoCandidatura() {
+        return desSituacaoCandidatura;
+    }
+
+    public void setDesSituacaoCandidatura(String desSituacaoCandidatura) {
+        this.desSituacaoCandidatura = desSituacaoCandidatura;
+    }
+
+    public String getNumeroPartido() {
+        return numeroPartido;
+    }
+
+    public void setNumeroPartido(String numeroPartido) {
+        this.numeroPartido = numeroPartido;
+    }
+
+    public String getSiglaPartido() {
+        return siglaPartido;
+    }
+
+    public void setSiglaPartido(String siglaPartido) {
+        this.siglaPartido = siglaPartido;
+    }
+
+    public String getNomePartido() {
+        return nomePartido;
+    }
+
+    public void setNomePartido(String nomePartido) {
+        this.nomePartido = nomePartido;
+    }
+
+    public String getCodigoLegenda() {
+        return codigoLegenda;
+    }
+
+    public void setCodigoLegenda(String codigoLegenda) {
+        this.codigoLegenda = codigoLegenda;
+    }
+
+    public String getSiglaLgenda() {
+        return siglaLgenda;
+    }
+
+    public void setSiglaLgenda(String siglaLgenda) {
+        this.siglaLgenda = siglaLgenda;
+    }
+
+    public String getComposicaoLegenda() {
+        return composicaoLegenda;
+    }
+
+    public void setComposicaoLegenda(String composicaoLegenda) {
+        this.composicaoLegenda = composicaoLegenda;
+    }
+
+    public String getNomeLegenda() {
+        return nomeLegenda;
+    }
+
+    public void setNomeLegenda(String nomeLegenda) {
+        this.nomeLegenda = nomeLegenda;
+    }
+
+    public String getCodigoOcupacao() {
+        return codigoOcupacao;
+    }
+
+    public void setCodigoOcupacao(String codigoOcupacao) {
+        this.codigoOcupacao = codigoOcupacao;
+    }
+
+    public String getDescricaoOcupacao() {
+        return descricaoOcupacao;
+    }
+
+    public void setDescricaoOcupacao(String descricaoOcupacao) {
+        this.descricaoOcupacao = descricaoOcupacao;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNumTituloEleitoralCandidato() {
+        return numTituloEleitoralCandidato;
+    }
+
+    public void setNumTituloEleitoralCandidato(String numTituloEleitoralCandidato) {
+        this.numTituloEleitoralCandidato = numTituloEleitoralCandidato;
+    }
+
+    public String getIdadeDataEleicao() {
+        return idadeDataEleicao;
+    }
+
+    public void setIdadeDataEleicao(String idadeDataEleicao) {
+        this.idadeDataEleicao = idadeDataEleicao;
+    }
+
+    public String getCodigoSexo() {
+        return codigoSexo;
+    }
+
+    public void setCodigoSexo(String codigoSexo) {
+        this.codigoSexo = codigoSexo;
+    }
+
+    public String getDescricaoSexo() {
+        return descricaoSexo;
+    }
+
+    public void setDescricaoSexo(String descricaoSexo) {
+        this.descricaoSexo = descricaoSexo;
+    }
+
+    public String getCodGrauInstrucao() {
+        return codGrauInstrucao;
+    }
+
+    public void setCodGrauInstrucao(String codGrauInstrucao) {
+        this.codGrauInstrucao = codGrauInstrucao;
+    }
+
+    public String getDescricaoGrauInstrucao() {
+        return descricaoGrauInstrucao;
+    }
+
+    public void setDescricaoGrauInstrucao(String descricaoGrauInstrucao) {
+        this.descricaoGrauInstrucao = descricaoGrauInstrucao;
+    }
+
+    public String getCodigoEstadoCivil() {
+        return codigoEstadoCivil;
+    }
+
+    public void setCodigoEstadoCivil(String codigoEstadoCivil) {
+        this.codigoEstadoCivil = codigoEstadoCivil;
+    }
+
+    public String getDescricaoEstadoCivil() {
+        return descricaoEstadoCivil;
+    }
+
+    public void setDescricaoEstadoCivil(String descricaoEstadoCivil) {
+        this.descricaoEstadoCivil = descricaoEstadoCivil;
+    }
+
+    public String getCodigoCorRaca() {
+        return codigoCorRaca;
+    }
+
+    public void setCodigoCorRaca(String codigoCorRaca) {
+        this.codigoCorRaca = codigoCorRaca;
+    }
+
+    public String getDescricaoCorRaca() {
+        return descricaoCorRaca;
+    }
+
+    public void setDescricaoCorRaca(String descricaoCorRaca) {
+        this.descricaoCorRaca = descricaoCorRaca;
+    }
+
+    public String getCodigoNascionalidade() {
+        return codigoNascionalidade;
+    }
+
+    public void setCodigoNascionalidade(String codigoNascionalidade) {
+        this.codigoNascionalidade = codigoNascionalidade;
+    }
+
+    public String getDescricaoNascionalidade() {
+        return descricaoNascionalidade;
+    }
+
+    public void setDescricaoNascionalidade(String descricaoNascionalidade) {
+        this.descricaoNascionalidade = descricaoNascionalidade;
+    }
+
+    public String getSiglaUfNascimento() {
+        return siglaUfNascimento;
+    }
+
+    public void setSiglaUfNascimento(String siglaUfNascimento) {
+        this.siglaUfNascimento = siglaUfNascimento;
+    }
+
+    public String getCodigoMunicipioNascimento() {
+        return codigoMunicipioNascimento;
+    }
+
+    public void setCodigoMunicipioNascimento(String codigoMunicipioNascimento) {
+        this.codigoMunicipioNascimento = codigoMunicipioNascimento;
+    }
+
+    public String getNomeMunicipioNascimento() {
+        return nomeMunicipioNascimento;
+    }
+
+    public void setNomeMunicipioNascimento(String nomeMunicipioNascimento) {
+        this.nomeMunicipioNascimento = nomeMunicipioNascimento;
+    }
+
+    public String getDespesaMaxCampanha() {
+        return despesaMaxCampanha;
+    }
+
+    public void setDespesaMaxCampanha(String despesaMaxCampanha) {
+        this.despesaMaxCampanha = despesaMaxCampanha;
+    }
+
+    public String getCodSitTotTurno() {
+        return codSitTotTurno;
+    }
+
+    public void setCodSitTotTurno(String codSitTotTurno) {
+        this.codSitTotTurno = codSitTotTurno;
+    }
+
+    public String getDescSitTotTurno() {
+        return descSitTotTurno;
+    }
+
+    public void setDescSitTotTurno(String descSitTotTurno) {
+        this.descSitTotTurno = descSitTotTurno;
+    }
+
+    public String getNmEmail() {
+        return nmEmail;
+    }
+
+    public void setNmEmail(String nmEmail) {
+        this.nmEmail = nmEmail;
+    }
+
+    public String getDataHoraGeracao() {
+        return dataHoraGeracao;
+    }
+
+    public void setDataHoraGeracao(String dataHoraGeracao) {
+        this.dataHoraGeracao = dataHoraGeracao;
+    }
 }
