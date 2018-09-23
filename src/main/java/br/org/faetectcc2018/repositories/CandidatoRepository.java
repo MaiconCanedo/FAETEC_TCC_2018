d710c760cb61dd698e76cf0998176b84411d1efc
Maicon Canedo <maicon.canedo2013@outlook.com>
2018-09-22 22:15:03 -0300

    editando .gitignore

--- src/main/java/br/org/faetectcc2018/repositories/CandidatoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.org.faetectcc2018.repositories;
-
-import br.org.faetectcc2018.model.Candidato;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface CandidatoRepository extends PagingAndSortingRepository<Candidato, Long> {
-
-}
