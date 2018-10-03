f8de5d35ff840ff0271e6a9aea7d8978d55c3c1e
Maicon Canedo <maicon.rocha2013@outlook.com>
2018-10-03 18:11:10 -0300

    Implemantando Documentação Swagger no projeto

---
+++ src/main/java/br/org/faetectcc2018/config/SwaggerConfig.java
@@ -0,0 +1,56 @@
+package br.org.faetectcc2018.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private final ResponseMessage m404 = responseMessage(404, "Não Encontrado");
+    private final ResponseMessage m403 = responseMessage(403, "Não Autorizado");
+    private final ResponseMessage m401 = responseMessage(401, "Não Autorizado");
+
+    private ResponseMessage responseMessage(Integer code, String message) {
+        return new ResponseMessageBuilder().code(code).message(message).build();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET, Arrays.asList(m401, m403, m404))
+
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.org.faetectcc2018.resources"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "TCC FAETEC TI 2018",
+                "TRABALHO DE CONCLUSÃO DE CURSO FAETEC TI 2018",
+                "Versão 1.0",
+                "https://github.com/MaiconCanedo/FAETEC_TCC_2018",
+                new Contact("Maicon Canedo", "https://github.com/MaiconCanedo/FAETEC_TCC_2018.git", "maicon.canedo2013@outlook.com"),
+                "Permitido uso para estudadentes",
+                "https://github.com/MaiconCanedo/FAETEC_TCC_2018",
+                Collections.emptyList()
+        );
+    }
+}
--- src/main/java/br/org/faetectcc2018/resources/CandidatoResource.java
+++ src/main/java/br/org/faetectcc2018/resources/CandidatoResource.java
@@ -2,6 +2,7 @@ package br.org.faetectcc2018.resources;
 
 import br.org.faetectcc2018.model.Candidato;
 import br.org.faetectcc2018.services.CandidatoService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ public class CandidatoResource {
     @Autowired
     private CandidatoService service;
 
+    @ApiOperation(value = "Retorna todos os candidatos com paginação")
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<Page<Candidato>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
                                                     @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
@@ -24,6 +26,7 @@ public class CandidatoResource {
         return ResponseEntity.ok(service.findPage(page, linesPerPage, orderBy, direction));
     }
 
+    @ApiOperation(value = "Retorna um candidato")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<Candidato> find(@PathVariable Long id) {
         return ResponseEntity.ok(service.find(id));
