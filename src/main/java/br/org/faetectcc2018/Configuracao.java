63a166dca3726e28dec5dc6a1e19f88c17eeb648
Maicon Canedo <maicon.canedo2013@outlook.com>
2018-10-27 16:42:42 -0300

    refactor: Atualizando reorganizando e limpando dependencias

--- src/main/java/br/org/faetectcc2018/Configuracao.java
+++ src/main/java/br/org/faetectcc2018/Configuracao.java
@@ -1,5 +1,5 @@
-package br.org.faetectcc2018;
 
+package br.org.faetectcc2018;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
